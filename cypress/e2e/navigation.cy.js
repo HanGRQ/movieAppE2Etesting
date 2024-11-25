56eb5092fe22906a1a2a6c91fe13ee73b8c1c778
Sihan Ma <masihan0303@gmail.com>
2024-11-25 16:44:28 +0000

    E2E tests - Exercise - Navigating between Favourites page and movie details page.

--- cypress/e2e/navigation.cy.js
+++ cypress/e2e/navigation.cy.js
@@ -31,6 +31,23 @@ describe("Navigation", () => {
     });
   });
   describe("From the favourites page to a movie's details", () => {
-    // TODO
+    before(() => {
+      // Add a movie to favorites
+      movieId = movies[0].id; // Use the first movie in the list
+      cy.get(".MuiCardActions-root").eq(0).contains("Add to Favorites").click();
+    });
+  
+    it("Should navigate to the movie details page and change the browser's URL", () => {
+      // Navigate to the Favourites page
+      cy.get("button").contains("Favorites").click();
+      cy.url().should("include", "/favorites");
+  
+      // Click on the "More Info" link of the first favorite movie
+      cy.get(".MuiCardActions-root").eq(0).contains("More Info").click();
+  
+      // Assert the URL has changed to the movie details page
+      cy.url().should("include", `/movies/${movieId}`);
+    });
   });
+  
 });
